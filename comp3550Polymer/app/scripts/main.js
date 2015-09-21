c928d22e9138b2a78e93ad3872af51974e76ae24
jevonalexis <jevon.alexis@gmail.com>
2015-09-20 20:20:44 -0400

    99% finished

--- comp3550Polymer/app/scripts/main.js
+++ comp3550Polymer/app/scripts/main.js
@@ -1,21 +1,21 @@
 ////////////////////// posting form ///////////////////////
 
-function clearFields(id){
-	document.getElementById(id).reset();
+
+function clearFields(formid){
+    $("#"+formid).find("input, textarea").val("");
+
 }
 
-
-function postToPage(name, country, comment){
-	var commentDiv = document.getElementById("commentDiv");
+//accepts a user object and a div id and posts posts the contents of that
+//user object to a new card withing the specified div
+function postToPage(user,div){
+	var commentDiv = document.getElementById(div);
 	var paperEl = document.createElement('paper-material');
 
 	paperEl.elevation = 1;
 	paperEl.class = "newComment";
-	paperEl.innerHTML = "<h3> " + name + " of " + country + " </h3> <p>" + comment + "</p>";
-	commentDiv.appendChild(paperEl);
-	toast = document.getElementById('toast');
-	toast.show();
-
+	paperEl.innerHTML = "<h3> " + user.uname + " of " + user.country + " </h3> <p>" + user.comment + "</p>";
+	commentDiv.insertBefore(paperEl,commentDiv.childNodes[0]);
 }
 
 //uppercases the first word of a string
@@ -28,6 +28,8 @@ function toUpperFW( str ){
     return pieces.join(" ");
 }
 
+
+//validates the form for posting to page and executes the post function 
 function handleForm(){
 	console.log("Submit clicked");
 	var form = document.forms[0];
@@ -37,7 +39,7 @@ function handleForm(){
 	var comment = form["ti_comment"].value;
 
 	if(name === ""){
-		alert("Let YOUR voice be heard. Please enter your name.");
+		alert("Let YOUR voice be hear. Please enter your name.");
 	}
 	else if(country === ""){
 		alert("Be the voice of YOUR country. Please state your country.");
@@ -45,18 +47,23 @@ function handleForm(){
 	else if(comment === ""){
 		alert("Oops looks like you forgot to put a comment.");
 	}
-
 	else{
-		postToPage(toUpperFW(name), toUpperFW(country), toUpperFW(comment));
+		var user = {};
+		user.uname=toUpperFW(name);
+		user.country=toUpperFW(country);
+		user.comment=toUpperFW(comment);
+		postToPage(user,'commentDiv');
 		clearFields('comment-form');
 	}
 	return false;
 }
 
+
+
 ///////////////////////////// sign up form //////////////////////////
 
-function showSignupToast(){
-	toast = document.getElementById('signuptoast');
+function showToast(id){
+	toast = document.getElementById(id);
 	toast.show();
 	return false;
 }
@@ -104,8 +111,58 @@ function handleForm2(){
 		return false;
 	}
 	else{
-		showSignupToast();
+		showToast('signuptoast');
 		clearFields('signup-form');
 		return true;
 	}
 }
+
+
+////////////////// AJAX to load older posts ////////////////////////////
+
+var loaded = false;
+function loadOldComments(){
+
+	if(!loaded){
+		console.log("btn clickeddd");
+		var xmlhttp;
+		if (window.XMLHttpRequest)
+		  {// code for IE7+, Firefox, Chrome, Opera, Safari
+		  xmlhttp=new XMLHttpRequest();
+		  }
+		else{// code for IE6, IE5
+		  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		xmlhttp.onreadystatechange=function(){
+		  if (xmlhttp.readyState==4 && xmlhttp.status==200){
+		    	processFile(xmlhttp.responseText);
+		    	loaded = true;
+		   }
+		}
+		xmlhttp.open("GET","older_posts.txt",true);
+		xmlhttp.send();
+	}
+	else{
+		showToast('no-post');
+	}
+}
+
+
+
+function processFile(fileTxt){
+	if(fileTxt!=""){
+		var arr="";
+		arr = fileTxt.split(">>>");	
+		processArray(arr);
+	}
+}
+
+function processArray(arr){
+	var i;
+	for(i = 0; i <= arr.length-3; i+=3){
+		var user ={uname:arr[i], country:arr[i+1], comment:arr[i+2]};
+		postToPage(user,'oldCommentDiv');
+	}
+}
+
+
